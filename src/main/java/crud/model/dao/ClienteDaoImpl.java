cfc11c988abafbd34c282c08731bf7ec09f58bc8
fjuribe <fjuribeui@gmail.com>
2019-10-17 13:13:39 -0300

    Agregando entity y estructura de clases dao

---
+++ src/main/java/crud/model/dao/ClienteDaoImpl.java
@@ -0,0 +1,15 @@
+package crud.model.dao;
+
+import java.util.List;
+
+import crud.model.entity.Cliente;
+
+public class ClienteDaoImpl implements IClienteDao{
+
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/crud/model/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package crud.model.dao;
+
+import java.util.List;
+
+import crud.model.entity.Cliente;
+
+public interface IClienteDao {
+
+	public List<Cliente> findAll();
+}
