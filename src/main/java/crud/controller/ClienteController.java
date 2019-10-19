72134451641bd2018789b37057db616d885efc70
fjuribe <fjuribeui@gmail.com>
2019-10-19 16:07:05 -0300

    Creando la vita y formulario

--- src/main/java/crud/controller/ClienteController.java
+++ src/main/java/crud/controller/ClienteController.java
@@ -1,5 +1,7 @@
 package crud.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import crud.model.dao.IClienteDao;
+import crud.model.entity.Cliente;
 
 @Controller
 public class ClienteController {
@@ -21,6 +24,21 @@ public class ClienteController {
 		model.addAttribute("titulo","listado de clientes");
 		model.addAttribute("clientes",clienteDao.findAll());
 		return "listar";
+	}
+	
+	@RequestMapping(value = "/form")
+	public String crear(Map<String,Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("cliente",cliente);
+		model.put("titulo","formulario de cliente");
+		return "form";
+	}
+	
+	@RequestMapping(value = "/form",method =RequestMethod.POST)
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
+		
 	}
 	
 	
--- src/main/java/crud/model/dao/IClienteDao.java
+++ src/main/java/crud/model/dao/IClienteDao.java
@@ -7,4 +7,6 @@ import crud.model.entity.Cliente;
 public interface IClienteDao {
 
 	public List<Cliente> findAll();
+	
+	public void save(Cliente cliente);
 }
