2636cdee45e5d1b4694ce0db5156656d45e4ba5d
fjuribe <fjuribeui@gmail.com>
2019-10-19 15:26:26 -0300

    Agregando bootstrap

---
+++ src/main/java/crud/controller/ClienteController.java
@@ -0,0 +1,27 @@
+package crud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import crud.model.dao.IClienteDao;
+
+@Controller
+public class ClienteController {
+
+	@Autowired
+	@Qualifier("clienteDaoJpa")
+	private IClienteDao clienteDao;
+	
+	@RequestMapping(value="/listar",method = RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("titulo","listado de clientes");
+		model.addAttribute("clientes",clienteDao.findAll());
+		return "listar";
+	}
+	
+	
+}
--- src/main/java/crud/model/dao/ClienteDaoImpl.java
+++ src/main/java/crud/model/dao/ClienteDaoImpl.java
@@ -1,15 +1,23 @@
 package crud.model.dao;
-
 import java.util.List;
-
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import crud.model.entity.Cliente;
 
+@Repository("clienteDaoJpa")
 public class ClienteDaoImpl implements IClienteDao{
 
+	@PersistenceContext 
+	private EntityManager em;
+	
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
 	@Override
 	public List<Cliente> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return em.createQuery("from Cliente").getResultList();
 	}
 
 }
