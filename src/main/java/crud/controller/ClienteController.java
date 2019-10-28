3d872d4d007b97a8285a0fa6a0b64ba78e4606e3
fjuribe <fjuribeui@gmail.com>
2019-10-28 19:12:48 -0200

    Agregar directorio uploads externo al proyecto (Resource Handler)

--- src/main/java/crud/controller/ClienteController.java
+++ src/main/java/crud/controller/ClienteController.java
@@ -84,8 +84,8 @@ public class ClienteController {
 			return "form";
 		}
 		if(!foto.isEmpty()) {
-			Path directorioRecursos=Paths.get("src//main/resources//static/uploads");
-			String rootPath=directorioRecursos.toFile().getAbsolutePath();
+			
+			String rootPath="C://spring5//uploads";
 			try {
 				byte[] bytes=foto.getBytes();
 				//ruta final 
