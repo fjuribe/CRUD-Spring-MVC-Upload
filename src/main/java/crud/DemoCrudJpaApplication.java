3b2235061e01979507d89f59728f0e651acfbd15
fjuribe <fjuribeui@gmail.com>
2019-10-29 00:47:01 -0300

    Iniciando directorio uploads automaticamente usando CommandLineRunner

--- src/main/java/crud/DemoCrudJpaApplication.java
+++ src/main/java/crud/DemoCrudJpaApplication.java
@@ -1,13 +1,27 @@
 package crud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoCrudJpaApplication {
+import crud.model.service.IUploadFileService;
 
+@SpringBootApplication
+public class DemoCrudJpaApplication implements CommandLineRunner{
+
+	@Autowired
+	IUploadFileService uploadFileService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCrudJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		uploadFileService.init();
+		
 	}
 
 }
