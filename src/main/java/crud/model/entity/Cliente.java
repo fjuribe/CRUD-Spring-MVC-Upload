093fd568418e1d640d73e3dd31912b17b5a6e50b
fjuribe <fjuribeui@gmail.com>
2019-11-11 02:07:38 -0300

    Creando la vista ver detalles - Parte 1 . Datos de la factura

--- src/main/java/crud/model/entity/Cliente.java
+++ src/main/java/crud/model/entity/Cliente.java
@@ -134,5 +134,10 @@ public class Cliente implements Serializable {
 	public void addFactura(Factura factura) {
 		facturas.add(factura);
 	}
+
+	@Override
+	public String toString() {
+		return nombre+" "+apellido;
+	}
 	
 }
