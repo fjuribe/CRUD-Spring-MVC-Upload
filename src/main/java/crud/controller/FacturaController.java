27984f21e4073ad04d7716f78c983723e387046d
fjuribe <fjuribeui@gmail.com>
2019-11-11 00:15:44 -0300

    Implementando metodo handler guardar en Facturacontroller

--- src/main/java/crud/controller/FacturaController.java
+++ src/main/java/crud/controller/FacturaController.java
@@ -3,17 +3,23 @@ package crud.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import crud.model.entity.Cliente;
 import crud.model.entity.Factura;
+import crud.model.entity.ItemFactura;
 import crud.model.entity.Producto;
 import crud.model.service.IClienteService;
 
@@ -24,6 +30,9 @@ public class FacturaController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	
+	private final Logger log=LoggerFactory.getLogger(getClass());
 	
 	@GetMapping("/form/{clienteId}")
 	public String crear(@PathVariable(value="clienteId") Long clienteId,Map<String,Object> model,RedirectAttributes flash) {
@@ -43,6 +52,33 @@ public class FacturaController {
 	@GetMapping(value="/cargar-productos/{term}",produces = {"application/json"})
 	public @ResponseBody List<Producto> cargarProducto(@PathVariable String term){
 		return clienteService.findByNombre(term);
+	}
+	
+	
+	@PostMapping("/form")
+	public String guardar(Factura factura,
+			@RequestParam(name="item_id[]",required = false) Long[] itemId,
+			@RequestParam(name="cantidad[]",required = false) Integer[] cantidad,
+			RedirectAttributes flash,
+			SessionStatus status) {
+		
+		for(int i=0;i<itemId.length;i++) {
+			Producto producto=clienteService.findProductoById(itemId[i]);
+			
+			ItemFactura linea=new ItemFactura();
+			linea.setCantidad(cantidad[i]);
+			linea.setProducto(producto);
+			factura.addItemFactura(linea);
+			
+			log.info("ID:" +itemId[i].toString()+", cantidad:"+cantidad[i].toString());
+		}
+		clienteService.saveFactura(factura);
+		
+		status.setComplete();
+		
+		flash.addFlashAttribute("success","Factura creada con exito");
+		
+		return "redirect:/ver/"+factura.getCliente().getId();
 	}
 
 }
